875a7ff0e80b66c699cac2e119075f9d74a40c65
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-07-11 09:32:50 +0530

    added code for email validation

--- js/SImpleForm.js
+++ js/SImpleForm.js
@@ -1,14 +1,16 @@
- const text = document.querySelector('#text');
- const textError = document.querySelector('.text-error');
-        text.addEventListener('input', function () {
-            let nameRegex = RegExp('^[A-Z]{1}[a-zA-Z]{2,}$');
-            if (nameRegex.test(text.value))
-                textError.textContent = "";
-            else textError.textContent = "Name is incorrect";
-        });
-        const salary = document.querySelector('#salary');
-        const output = document.querySelector('.salary-output');
-        output.textContent = salary.value;
-        salary.addEventListener('input', function () {
-            output.textContent = salary.value;
-        });
+ // Code For Salary
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary-output');
+output.textContent = salary.value;
+salary.addEventListener('input', function () {
+    output.textContent = salary.value;
+});
+// Validation For Name
+const text= document.querySelector('#text');
+const textError = document.querySelector('.text-error');
+text.addEventListener('input',function(){
+let nameRgx = RegExp('^[A-Z]{1}[a-z]{2,}$');
+if(nameRgx.test(text.value))
+textError.textContent= "";
+else textError.textContent = "Name Is Incorrect";
+});
