e08078b4633c5f420d0c67df10b6e02853ba5c4c
gayuchowdhary <gayuchowdhary51@gmail.com>
2022-07-11 09:39:15 +0530

    added code for phonenum validation

--- js/SImpleForm.js
+++ js/SImpleForm.js
@@ -14,3 +14,11 @@ if(nameRgx.test(text.value))
 textError.textContent= "";
 else textError.textContent = "Name Is Incorrect";
 });
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function(){
+    let telRgx = RegExp('^[0-9]{2}\\s{0,1}[0-9]{10}$');
+    if(telRgx.test(tel.value))
+    telError.textContent= "";
+    else telError.textContent = "Phone No Is Incorrect";
+});
